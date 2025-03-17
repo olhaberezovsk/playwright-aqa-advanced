66ac0978cb6a6460a342b2d2b08cab364e07e0e2
Olha <berezovskaolh@gmail.com>
2025-03-17 13:48:14 +0200

    HomeWork#27 - added 4 files setup.ts, storageState.json, fixtures file and test file

---
+++ tests/fixtures/fixtures.ts
@@ -0,0 +1,18 @@
+import { test as base, Page } from '@playwright/test';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+export const test = base.extend<{ userGaragePage: Page }>({
+    userGaragePage: async ({ page }, use) => {
+    const loginUrl = 'https://guest:welcome2qauto@qauto.forstudy.space';
+
+    await page.goto(loginUrl);
+    await page.click('button.header-link:nth-child(1)');
+    
+    await page.waitForSelector('h1');  
+    await page.context().storageState({ path: 'storageState.json' });
+    await use(page);
+  }
+});
+
+export { expect } from '@playwright/test'
